e5b64051efd33e70dff6c6232a6955da19dde72f
ParadigmMango2 <jacobjhartt@gmail.com>
2025-05-02 20:35:20 -0600

    ensure directories

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import os
 import xdg_base_dirs
 
 
-RICEMGR_DIR_NAME = "ricemgr"
-RICEMGR_DIR = os.path.join(xdg_base_dirs.xdg_config_home(), RICEMGR_DIR_NAME)
+RICEMGR_DIR = "ricemgr"
+CONFIG_DIR = os.path.join(xdg_base_dirs.xdg_config_home(), RICEMGR_DIR)
+DATA_DIR = os.path.join(xdg_base_dirs.xdg_data_home(), RICEMGR_DIR)
 LOG_FILE = "ricemgr.log"
 
 logging.basicConfig(filename=LOG_FILE,
@@ -13,10 +14,21 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 
-if __name__ == "__main__":
-    if not os.path.exists(RICEMGR_DIR):
-        os.makedirs(RICEMGR_DIR)
+def ensure_config_dir():
+    if not os.path.exists(CONFIG_DIR):
+        os.makedirs(CONFIG_DIR)
         logger.info("Built config directory")
+
+
+def ensure_data_dir():
+    if not os.path.exists(DATA_DIR):
+        os.makedirs(DATA_DIR)
+        logger.info("Built data directory")
+
+
+if __name__ == "__main__":
+    ensure_config_dir()
+    ensure_data_dir()
 
     print(RICEMGR_DIR)
     print("Hello World")
